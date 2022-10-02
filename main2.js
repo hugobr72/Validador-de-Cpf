8c8747b715b1025279c857f9bc8d5c9c5d18667d
hugobr72 <hugobrod72@gmail.com>
2022-10-02 08:49:00 -0300

    Refatornado o codigo Js

--- main2.js
+++ main2.js
@@ -7,28 +7,11 @@ function Validador() {
     const replaceCpf = this.replace(this.cpf.value)
     var soma = 0
     var resto = 0
+    var cont = 11
 
     for (c in replaceCpf) {
-      if (c == 0) soma = soma + (replaceCpf[c] * 11)
-
-      else if (c == 1) soma = soma + (replaceCpf[c] * 10)
-
-      else if (c == 2) soma = soma + (replaceCpf[c] * 9)
-
-      else if (c == 3) soma = soma + (replaceCpf[c] * 8)
-
-      else if (c == 4) soma = soma + (replaceCpf[c] * 7)
-
-      else if (c == 5) soma = soma + (replaceCpf[c] * 6)
-
-      else if (c == 6) soma = soma + (replaceCpf[c] * 5)
-
-      else if (c == 7) soma = soma + (replaceCpf[c] * 4)
-
-      else if (c == 8) soma = soma + (replaceCpf[c] * 3)
-
-      else if (c == 9) soma = soma + (replaceCpf[c] * 2)
-
+      if (c <= 8 ) soma = soma + (replaceCpf[c] * cont)
+      cont -=1
     }
 
     resto = (soma * 10) % 11
